7479e75a00f6777f81760c4c76a80e5fb310e418
CoCarrivale <carrivale.costanza@yahoo.it>
2024-06-10 17:35:00 +0200

    config update

---
+++ out_zhjj/bin/internal/ufomodel/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.14
+# Mathematica version: 10.3.0 for Linux x86 (64-bit) (October 9, 2015)
+# Date: Fri 6 Mar 2020 14:41:44
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ out_zhjj/bin/internal/ufomodel/coupling_orders.py
@@ -0,0 +1,100 @@
+# This file was automatically created by FeynRules 2.3.14
+# Mathematica version: 10.3.0 for Linux x86 (64-bit) (October 9, 2015)
+# Date: Fri 6 Mar 2020 14:41:44
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+M0 = CouplingOrder(name = 'M0',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M1 = CouplingOrder(name = 'M1',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M2 = CouplingOrder(name = 'M2',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M3 = CouplingOrder(name = 'M3',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M4 = CouplingOrder(name = 'M4',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M5 = CouplingOrder(name = 'M5',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M6 = CouplingOrder(name = 'M6',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+M7 = CouplingOrder(name = 'M7',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+S0 = CouplingOrder(name = 'S0',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+S1 = CouplingOrder(name = 'S1',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+S2 = CouplingOrder(name = 'S2',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T0 = CouplingOrder(name = 'T0',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T1 = CouplingOrder(name = 'T1',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T2 = CouplingOrder(name = 'T2',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T3 = CouplingOrder(name = 'T3',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T4 = CouplingOrder(name = 'T4',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T5 = CouplingOrder(name = 'T5',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T6 = CouplingOrder(name = 'T6',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T7 = CouplingOrder(name = 'T7',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T8 = CouplingOrder(name = 'T8',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+T9 = CouplingOrder(name = 'T9',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
