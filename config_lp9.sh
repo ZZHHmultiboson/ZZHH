90af81ea3291f4e84719a02118983eb8b1731151
CoCarrivale <carrivale.costanza@yahoo.it>
2024-06-20 12:02:17 +0200

    update config

--- config_lp9.sh
+++ config_lp9.sh
@@ -37,17 +37,6 @@ echo $pyPath
 
 find . -type f -exec sed -i "s|/afs/cern.ch/user/c/ccarriva/ZZHH|$baseDir|g" {} +
 find . -type f -exec sed -i "s|afs', 'cern.ch', 'user', 'c', 'ccarriva', 'ZZHH'|$pyPath|g" {} +
-#sed -i "s|afs', 'cern.ch', 'user', 'c', 'ccarriva', 'ZZHH'|$pyPath|g" plot_and_compute_fractions_checkCuts.py
-#sed -i "s|afs', 'cern.ch', 'user', 'c', 'ccarriva', 'ZZHH'|$pyPath|g" plot_only.py
-
-#cd ../MG5_aMC_v2_9_18
-#cp ../scripts/condor/MG5config_generic_cuts.txt .
-#cp ../scripts/condor/copyandsend_generic_cuts.sh .
-#cp ../scripts/condor/condor_cuts.sub .
-#cp ../scripts/condor/sendOne_cuts.sh .
-#cp ../scripts/createCsv.sh .
-#cp ../scripts/my_analyzer_cuts.py .
-#cp ../scripts/run_madgraph.sh .
 
 # Plots repository
 echo "Setting up plotting enviroment..."
